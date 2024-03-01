1ff6d9254f0f053d24dafead8b5ea3b15b07568b
Dekhsa Afnan <dekhsafnan@gmail.com>
2024-03-01 22:26:44 +0700

    Adding raw-dynamic-array.cpp

---
+++ dynamic-array/raw-dynamic-array.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+struct dynamicarr_t{
+   int *arr;
+   unsigned _size, _capacity;
+}DynamicArray;
+
+void dArray_pushBack(DynamicArray *darray, int value){
+   if(darray->_size +1 > darray->capacity){
+
+   }
+
+}
+
+int main (){
+
+
+}
