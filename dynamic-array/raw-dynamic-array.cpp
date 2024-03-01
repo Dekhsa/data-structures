351122efd48be8ff343253c830c69f5e7e50c3aa
Dekhsa Afnan <dekhsafnan@gmail.com>
2024-03-01 22:29:49 +0700

    update raw-dynamic-array.cpp

--- dynamic-array/raw-dynamic-array.cpp
+++ dynamic-array/raw-dynamic-array.cpp
@@ -1,17 +1,27 @@
 #include <iostream>
 using namespace std;
 
-struct dynamicarr_t{
+typedef struct dynamicarr_t{
    int *arr;
    unsigned _size, _capacity;
 }DynamicArray;
 
-void dArray_pushBack(DynamicArray *darray, int value){
-   if(darray->_size +1 > darray->capacity){
+void dArray_pushBack(DynamicArray *darray, int value)
+{
+    if (darray -> _size + 1 > darray -> _capacity){
+      // ingredients
+      unsigned it;
+      darray -> _capacity *= 2;
+      int *newArr = (int*)malloc(sizeof(int) * darray->_capacity);
 
-   }
+      for (it = 0; it < darray -> _size; ++it){
+         newArr[it] = darray -> arr[it];
+      }
 
+    }
 }
+
+
 
 int main (){
 
