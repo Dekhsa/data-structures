d625a25cdaa7e8261939abffe412a028c5fe119d
Dekhsa Afnan <dekhsafnan@gmail.com>
2024-03-02 22:25:55 +0700

    Added pushBack

--- vs-linked-list/raw-linked-list.cpp
+++ vs-linked-list/raw-linked-list.cpp
@@ -1,9 +1,37 @@
 #include<iostream>
 
-using namespace std;
+struct List {
+	int data = 0;
+	List* link = NULL;
+};
+
+bool List_empty(List* head) {
+	if (head == NULL)
+		return false;
+	return true;
+}
+
+void List_pushBack(List* head, int val) {
+	List* ptr, * temp;
+
+	ptr = head;
+	temp = (List*)malloc(sizeof(List));
+	temp->data = val;
+	temp->link = NULL;
+	while (ptr->link != NULL)
+		ptr = ptr->link;
+	ptr->link = temp;
+}
+
+void List_pushFront(List* head, int val) {
+
+}
 
 int main() {
-	cout << "Hello World!!" << endl;
+	List* head = (List*)malloc(sizeof(List));
+	head->data = 10;
+	
+	std::cout << List_empty(head) << std::endl;;
 
 	return 0;
 }
