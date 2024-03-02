ad204f8f313e3e0a26e209781a0ba7393732ebbe
Dekhsa Afnan <dekhsafnan@gmail.com>
2024-03-02 16:54:40 +0700

    finished raw-dynamic-array.cpp functions

--- dynamic-array/raw-dynamic-array.cpp
+++ dynamic-array/raw-dynamic-array.cpp
@@ -6,10 +6,13 @@ typedef struct dynamicarr_t{
    unsigned _size, _capacity;
 }DynamicArray;
 
+bool dArray_isEmpty(DynamicArray *darray){
+   return (darray->_size == 0);
+}
+
 void dArray_pushBack(DynamicArray *darray, int value)
 {
     if (darray -> _size + 1 > darray -> _capacity){
-      // ingredients
       unsigned it;
       darray -> _capacity *= 2;
       int *newArr = (int*)malloc(sizeof(int) * darray->_capacity);
@@ -18,12 +21,50 @@ void dArray_pushBack(DynamicArray *darray, int value)
          newArr[it] = darray -> arr[it];
       }
 
+      int *oldArray = darray -> arr;
+      darray -> arr = newArr;
+      free(oldArray);
     }
+   darray -> arr[darray->_size++] = value;
 }
 
+void dArray_popBack(DynamicArray *darray){
+   if(!dArray_isEmpty(darray))
+      darray->_size--;
+   else return;
+}
 
+int dArray_back(DynamicArray *darray){
+   if(!dArray_isEmpty(darray))
+      return darray -> arr[darray->_size--];
+   else return;
+}
+
+int dArray_front(DynamicArray *darray){
+   if(!dArray_isEmpty(darray))
+      return darray->arr[0];
+   else return;
+}
+
+void dArray_setAt(DynamicArray *darray, unsigned index, int value){
+   if(!dArray_isEmpty(darray))
+      if(index >= darray->_size)
+         darray->arr[darray->_size--] = value;
+      else 
+         darray->arr[index] = value;
+   else return;
+}
+
+int dArray_getAt(DynamicArray *darray, unsigned index){
+   if(!dArray_isEmpty(darray))
+      if(index>= darray->_size)
+         return darray->arr[darray->_size--];
+      else 
+         return darray->arr[index];
+   else return;
+}
 
 int main (){
-
+   
 
 }
